0768e179c8329fbd4974d209e6194193e11951b5
Liyobor <a0963382999@gmail.com>
2024-03-26 23:52:29 +0800

    Make code usable with command line input.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from Adpcm3 import Adpcm3
 from pydub import AudioSegment
 from adpcm import Adpcm
@@ -8,12 +9,12 @@ from tqdm import tqdm
 from multiprocessing import Pool
 
 
-def adpcmProcess(file):
-    # for file in tqdm(explorer.files):
-    if(os.path.exists(f'D:/adpcmed/{os.path.split(file)[1]}')):
+def adpcmProcess(info):
+    file_path,output_dir = info
+    if(os.path.exists(os.path.join(output_dir,file_path)[1])):
         return
     adpcm = Adpcm()
-    sound = AudioSegment.from_wav(file)
+    sound = AudioSegment.from_wav(file_path)
     sound = sound.set_channels(1)
     sound = sound.set_sample_width(2)
     dataArray = sound.get_array_of_samples()
@@ -38,16 +39,17 @@ def adpcmProcess(file):
         afterAdpcmData.append(adpcm.decode(data))
     afterAdpcmData = np.array(afterAdpcmData)
 
-    if not os.path.exists('D:/adpcmed'):
-        os.makedirs('D:/adpcmed')
-    wavf.write(f'D:/adpcmed/{os.path.split(file)[1]}', sound.frame_rate, afterAdpcmData.astype(np.int16))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir,exist_ok=True)
+    wavf.write(os.path.join(output_dir,file_path)[1], sound.frame_rate, afterAdpcmData.astype(np.int16))
 
 
-def adpcm3Process(file):
-    if(os.path.exists(f'D:/adpcmed/{os.path.split(file)[1]}')):
+def adpcm3Process(info):
+    file_path,output_dir = info
+    if(os.path.exists(os.path.join(output_dir,file_path)[1])):
         return
     adpcm3 = Adpcm3()
-    sound = AudioSegment.from_wav(file)
+    sound = AudioSegment.from_wav(file_path)
     sound = sound.set_channels(1)
     sound = sound.set_sample_width(2)
     dataArray = sound.get_array_of_samples()
@@ -70,24 +72,48 @@ def adpcm3Process(file):
         '''
         afterAdpcmData.append(adpcm3.decode(data))
     afterAdpcmData = np.array(afterAdpcmData)
-    # print(afterAdpcmData)
-    
-    if not os.path.exists('D:/adpcmed'):
-        os.makedirs('D:/adpcmed')
-    wavf.write(f'D:/adpcmed/{os.path.split(file)[1]}', sound.frame_rate, afterAdpcmData.astype(np.float32))
 
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir,exist_ok=True)
+    wavf.write(os.path.join(output_dir,file_path)[1], sound.frame_rate, afterAdpcmData.astype(np.float32))
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--files_path",
+        type=str,
+        default=None,
+        help="The path of the wav file.",
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default='D:/adpcmed/',
+        help="The path of the wav file.",
+    )
+
+
+    args = parser.parse_args()
+
+
+    if args.output_dir is not None:
+        os.makedirs(args.output_dir, exist_ok=True)
+
+    return args
 
 
 if __name__ == '__main__':
     
+    args = parse_args()
 
-    path = input("輸入音檔資料夾路徑\n")
-    
-    fileListTemp = os.listdir(path)
-    fileList = []
+    fileListTemp = os.listdir(args.files_path)
+    infoList = []
     for index in range(len(fileListTemp)):
-        if fileListTemp[index][-3:]=="wav":
-            fileList.append(os.path.join(path,fileListTemp[index]))
+        if fileListTemp[index].endswith("wav"):
+            infoList.append((os.path.join(args.files_path,fileListTemp[index]),args.output_dir))
 
     with Pool(os.cpu_count()) as pool:
-        list(tqdm(pool.imap(adpcm3Process,fileList),total=len(fileList)))
+        list(tqdm(pool.imap(adpcm3Process,infoList),total=len(infoList)))
